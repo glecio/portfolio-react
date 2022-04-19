7022491f3593794e5f2368ffa35af5b66f946c2a
Glecio Santos <glecio.santos@gmail.com>
2022-04-19 13:16:42 -0300

    Navigation paths

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+        <h5>Get to Know</h5>
+        <h2>About Me</h2>
+        <div className="container about__container">
+            <div className="about__me">
+                <img src={ME} alt="" />
+            </div>
+
+            <div className="about__content">
+
+            </div>
+        </div> 
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,9 +13,11 @@ const Header = () => {
         <h5 className="text-light">Front End Developer</h5>
         < CTA/>
         <HeaderSocials />
+        
         <div className="me">
           <img src={ME} alt="me" />
         </div>
+        
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
