5901e2f4a134e1eb5b4f3446291b228550e210cd
Glecio Santos <glecio.santos@gmail.com>
2022-04-29 17:12:14 -0300

    Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {ImInstagram} from 'react-icons/im'
+import {FaTwitterSquare} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>GTNI</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experiences">Experiences</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF /></a>
+        <a href="https://facebook.com"><ImInstagram /></a>
+        <a href="https://facebook.com"><FaTwitterSquare /></a>
+
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; GTNI Sites. All Rights REserved</small>
+      </div>
+    </footer>
   )
 }
 
